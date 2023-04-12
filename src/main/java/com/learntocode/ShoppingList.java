9f8a1069e1e2010106d82ec70429806eabe00eb0
ebellerand <ebellerand@appdev.yearup.org>
2023-04-12 11:30:42 -0400

    Add a new Shopping List class

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("eggs");
+        System.out.println("bread");
+        System.out.println("cheese");
+        System.out.println("Salt");
+        System.out.println("Pepper");
+        System.out.println("Bagels");
+        System.out.println("Chicken");
+        System.out.println("Turkey");
+        System.out.println("Fish");
+        System.out.println("Shark");
+    }
+}
